c3ee65fd99ae3d4e93e95b0de16482e5bff18e40
muzea <mr.muzea@gmail.com>
2018-12-28 13:23:58 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="background:#eee;padding: 20px">
+    <Card :bordered="false">
+      <p slot="title">Happy with parcel</p>
+      <p>Just a demo.</p>
+    </Card>
+  </div>
+</template>
+<script>
+export default {};
+</script>
